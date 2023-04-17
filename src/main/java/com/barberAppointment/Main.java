44277ef5b96d801fbabb4dcedb6c1581b0975ba4
yohelperez <perezyohel@gmail.com>
2023-04-17 06:58:23 -0500

    addition

--- src/main/java/com/barberAppointment/Main.java
+++ src/main/java/com/barberAppointment/Main.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         ConnectionDB.getConnectionDB();
-        System.out.println("Connected successfully...");
+        System.out.println("Connected successfully..");
 
     }
 }
